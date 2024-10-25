9a5c13d762d0e538517528abc3de25bff84b9c88
Marvin <32611659+msxdan@users.noreply.github.com>
2024-10-25 18:03:08 +0200

    update various files

--- dot_local/bin/executable_sc.sh
+++ dot_local/bin/executable_sc.sh
@@ -1,9 +1,16 @@
 #!/bin/bash
+# This script is used to perform a context switch, that means load a specific environment file instead of using a different user.
+# Its main purpose is to load env vars, custom aliases, etc.
+
+if [ -z "$1" ]; then
+  echo "Usage: sc <context>"
+  echo "Current valid contexts are: $(ls -1 ~/.private/.env.d/ | tr '\n' ' ')"
+fi
 
 env=~/.private/.env.d/$1
 
 if [ -f "$env" ]; then
   echo Loading $env
   source $env
-  export CONTEXT_ENV=$1
+  export CONTEXT_ENV="$1"
 fi
